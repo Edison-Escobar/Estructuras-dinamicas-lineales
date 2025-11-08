1dbca0c3f40a9e71bd4bf5105977b1025761d4df
Ediso <Ediso@LAPTOP-RGM4UVVR>
2025-11-08 15:16:13 -0600

    Agregar archivos de proyecto.

---
+++ Estructuras dinámicas lineales/Clases/DocState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras_dinámicas_lineales.Clases
+{
+    public class DocState
+    {
+        public string Text { get; set; }
+
+        public DocState{string text}
+
+    }
+}
---
+++ Estructuras dinámicas lineales/Form1.cs
@@ -0,0 +1,10 @@
+namespace Estructuras_dinámicas_lineales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
