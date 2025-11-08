c0632605cf42124cf976314dd8b1a56711632550
Edison-Escobar <adrianesco715@gmail.com>
2025-11-08 15:56:15 -0600

    Class node

--- Estructuras dinámicas lineales/HistoryList.cs
+++ Estructuras dinámicas lineales/HistoryList.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace Estructuras_dinámicas_lineales
 {
-    public class HistoryList
+    public class HistoryList<T>
     {
-        public class node
+        public class Node
         {
+            public T Value;
+            public Node prev;
+            public Node Next;
 
+            public Node(T value)
+            {
+                Value = value;
+            }
         }
     }
 }
